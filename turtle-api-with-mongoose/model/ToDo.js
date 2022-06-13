03b05a77e21cd507f177cb2c743f26797d0d7ac8
elistribley <eli_stribley@hotmail.com>
2022-06-13 10:54:56 +0100

    validation stuff ES

--- turtle-api-with-mongoose/model/ToDo.js
+++ turtle-api-with-mongoose/model/ToDo.js
@@ -4,10 +4,24 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const toDoSchema = new Schema({
-    title: String,
-    shortDesc: String,
-    category: String,
-    isDone: Boolean
+    title: {
+        type: String,
+        minlength: 3,
+        trim: true,
+        required: [true, 'Title is required']
+    },
+    shortDesc: {
+        type: String,
+        maxlength: 100
+    },
+    category: {
+        type: String
+    },
+    isDone: {
+        type: Boolean,
+        trim: true,
+        required: [true, 'isDone status is required']
+    }
 });
 
 const toDo = mongoose.model('ToDo', toDoSchema);
